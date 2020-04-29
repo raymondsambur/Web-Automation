9ca8f61f8de568ecbbd941da282d068ce8f0b42a
Raymond Sambur <raymond.sambur@dana.id>
2020-04-29 11:18:11 +0700

    Create Android Instance file

---
+++ src/main/java/demo/webdriver/AndroidDriverInstance.java
@@ -0,0 +1,11 @@
+package demo.webdriver;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class AndroidDriverInstance {
+
+    public static AndroidDriver<AndroidElement> androidDriver;
+
+
+}
